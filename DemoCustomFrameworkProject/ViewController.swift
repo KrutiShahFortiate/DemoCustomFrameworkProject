a4479e0e73cc4da77fe83d525696b2e128551170
KrutiShah_Fortiate <kruti@fortiate.com>
2022-05-24 15:38:09 +0530

    Swift package added

--- DemoCustomFrameworkProject/ViewController.swift
+++ DemoCustomFrameworkProject/ViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import DemoCustomFramework
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        DemoCustomFramework.SDK.doSomeWork()
     }
 
 
